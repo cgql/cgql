3c478acacf02114f16fe0307ad3fd1a90aeabf8c
codingwith3dv <deveshvdddas@gmail.com>
2021-11-18 14:14:14 +0530

    :sparkles: Add base template class for GraphQLTypes Add typedefs for custom types Add custom function type for serialization

---
+++ cgql/schema/GraphQLScalar.h
@@ -0,0 +1,25 @@
+#include "../utils/pch.h"
+
+#include <functional>
+
+using std::string;
+using std::function;
+
+template<typename T>
+using SerializeFunc = function<T(T)>;
+
+typedef int64_t Int;
+typedef std::string String;
+
+template<typename T>
+class GraphQLScalarType {
+public:
+  GraphQLScalarType(
+    const string& name,
+    const SerializeFunc<T>& serialize
+  ): name(name), serialize(serialize) {};
+  ~GraphQLScalarType() {};
+private:
+  string name;
+  SerializeFunc<T> serialize;
+};
