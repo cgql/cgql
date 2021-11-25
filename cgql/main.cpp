7677577798b6e45ffa9d010298f5d86145cfdd79
codingwith3dv <deveshvdddas@gmail.com>
2021-11-25 21:07:26 +0500

    :white_check_mark: All tests passed

--- cgql/main.cpp
+++ cgql/main.cpp
@@ -2,29 +2,34 @@
 #include "schema/GraphQLDefinition.h"
 
 int main() {
-  GraphQLObject address (
+  GraphQLObject address {
     "Address",
-    {
       {
-        "houseName",
-        GraphQLTypes::GraphQLString
-      },
-      {
-        "city",
-        GraphQLTypes::GraphQLString
+        {
+          "houseName",
+          GraphQLTypes::GraphQLString,
+          [](unordered_map<string, GraphQLInputTypes>) -> GraphQLOutputTypes {
+            return nullptr;
+          }
+        }
       }
-    }
-  );
+  };
   GraphQLObject person (
     "Person",
     {
       {
         "name",
-        GraphQLTypes::GraphQLString
+        GraphQLTypes::GraphQLString,
+        [](unordered_map<string, GraphQLInputTypes>) -> GraphQLOutputTypes {
+          return nullptr;
+        }
       },
       {
         "address",
-        &address
+        &address,
+        [](unordered_map<string, GraphQLInputTypes>) -> GraphQLOutputTypes {
+          return nullptr;
+        }
       }
     }
   );
