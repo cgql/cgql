42bc38d169147f40cff46c76def65c3444644529
codingwith3dv <deveshvdddas@gmail.com>
2021-11-25 10:21:10 +0500

    :recycle: Remove Object and Field from GraphQLDefinition.h

--- cgql/schema/GraphQLDefinition.h
+++ cgql/schema/GraphQLDefinition.h
@@ -1,22 +1,3 @@
 #include "../utils/pch.h"
-#include <unordered_map>
-#include <variant>
 
-#include "GraphQLScalarTypes.h"
-
-using std::string;
-using std::unordered_map;
-using std::variant;
-
-struct GraphQLField;
-struct GraphQLObject {
-  string name;
-  unordered_map<string, GraphQLField> fields;
-};
-struct GraphQLField {
-  variant<
-    GraphQLScalarType<Int>,
-    GraphQLScalarType<String>,
-    GraphQLObject
-  > type;
-};
+#include "GraphQLObject/GraphQLObject.h"
