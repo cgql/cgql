8f345b674b88c1a1a387bc7c08b37ab21d26f51d
codingwith3dv <deveshvdddas@gmail.com>
2021-11-25 21:06:04 +0500

    :sparkles: Update GraphQLField ( definition ) to accept resolver function

--- cgql/schema/GraphQLObject/GraphQLField.h
+++ cgql/schema/GraphQLObject/GraphQLField.h
@@ -1,8 +1,12 @@
 #include "../../utils/pch.h"
 #include "../GraphQLScalarTypes.h"
+#include <unordered_map>
 #include <variant>
- 
+#include <functional>
+
 using std::variant;
+using std::string;
+using std::unordered_map;
 
 class GraphQLObject;
 
@@ -12,14 +16,25 @@ using GraphQLOutputTypes = variant<
   GraphQLObject*
 >;
 
+using GraphQLInputTypes = variant<
+  GraphQLScalarType<Int>,
+  GraphQLScalarType<String>
+>;
+
+using ResolverFunc = function<
+  GraphQLOutputTypes(unordered_map<string, GraphQLInputTypes>)
+>;
+
 class GraphQLField {
 public:
   GraphQLField(
     string name,
-    GraphQLOutputTypes type
+    GraphQLOutputTypes type,
+    ResolverFunc resolve
   );
   ~GraphQLField();
 private:
   string name;
   GraphQLOutputTypes type;
+  ResolverFunc resolve;
 };
