241174e88679ecf0e10490c1bd05db949b055488
codingwith3dv <deveshvdddas@gmail.com>
2021-11-28 12:16:43 +0500

    :hammer: Update development script
    - Shows the time taken for the whole process

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
-cd build
-cmake ../cgql/ -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-make
-cp ./cgql ~/cgql-out/
+function build() {
+  cd build
+  cmake ../cgql/ -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+  make
+  cp ./cgql ~/cgql-out/
 
-if [ $1 -gt 0 ]
-then
   chmod 777 ~/cgql-out/cgql
-fi
-time ~/cgql-out/cgql
+  ~/cgql-out/cgql
+}
+
+time build
