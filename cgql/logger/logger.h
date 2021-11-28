d5352f114d22e96137f1ced87d544de5d6632f26
codingwith3dv <deveshvdddas@gmail.com>
2021-11-28 12:12:43 +0500

    :pencil2: Update color for success log

--- cgql/logger/logger.h
+++ cgql/logger/logger.h
@@ -1,10 +1,12 @@
 #include <iostream>
 namespace logger {
   #define INFO "\u001b[38;5;226m"
-  #define SUCCESS "\u001b[38;5;86m"
+  #define SUCCESS "\u001b[38;5;48m"
   #define ERROR "\u001b[38;5;196m"
   #define RESET "\u001b[0m"
 
   void print(const char* ansiCode, const char* msg);
   void info(const char* msg);
+  void success(const char* msg);
+  void error(const char* msg);
 };
