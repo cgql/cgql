49d6e83380918a9b02b63a397e3dac67bd9a5ed5
codingwith3dv <deveshvdddas@gmail.com>
2021-11-28 09:55:38 +0500

    :label: Add type definitions for Document, OperationDefinition, SelectionSet, Field

---
+++ cgql/type/Document.h
@@ -0,0 +1,54 @@
+#include <string>
+#include <variant>
+#include <vector>
+
+using std::vector;
+using std::variant;
+using std::string;
+
+enum OperationType {
+  QUERY,
+  MUTATION,
+  SUBSCRIPTION
+};
+
+class Field;
+
+using Selection = variant<
+  Field*
+>;
+typedef vector<Selection> SelectionSet;
+
+class Field {
+public:
+  Field(
+    string name,
+    const SelectionSet& selectionSet
+  );
+  ~Field();
+private:
+  string name;
+  SelectionSet selectionSet;
+};
+
+class OperationDefinition {
+public:
+  OperationDefinition(
+    OperationType operationType,
+    const SelectionSet& selectionSet
+  );
+  ~OperationDefinition();
+private:
+  OperationType operationType;
+  SelectionSet selectionSet;
+};
+
+class Document {
+public:
+  Document(
+    const vector<OperationDefinition>& definitions
+  );
+  ~Document();
+private:
+  vector<OperationDefinition> definitions;
+};
