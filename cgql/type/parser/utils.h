d80a7247f498f7aee03879b7195eb659c24cc887
codingwith3dv <deveshvdddas@gmail.com>
2021-12-02 14:58:34 +0500

    :pencil: Add utilities for Tokenizer

---
+++ cgql/type/parser/utils.h
@@ -0,0 +1,28 @@
+#include <string>
+
+using std::string;
+
+inline bool isLetter(uint32_t value) {
+  if(
+    (value >= 0x0041 && value <= 0x005A) ||
+    (value >= 0x0061 && value <= 0x007A)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+inline bool isDigit(uint32_t value) {
+  if(value >= 0x0030 && value <= 0x0039) {
+    return true;
+  }
+  return false;
+}
+
+inline bool isNameStart(uint32_t value) {
+  return isLetter(value) || value == 0x005F;
+}
+
+inline bool isNameContinue(uint32_t value) {
+  return isLetter(value) || isDigit(value) || value == 0x005F;
+}
