5982db3f7c17dc3308c579f0f0ebf6b7891e2def
codingwith3dv <deveshvdddas@gmail.com>
2021-12-02 14:59:18 +0500

    :file_folder: Add definitions for hand-written Top-Down Parser

---
+++ cgql/type/parser/parser.h
@@ -0,0 +1,28 @@
+#include "../Document.h"
+#include "tokenizer.h"
+#include <string>
+
+using std::string;
+
+class Parser {
+public:
+  Parser(const char* document);
+  ~Parser();
+  Document parseDocument();
+private:
+  Token move(TokenType type);
+  bool moveUntil(TokenType type);
+  bool checkType(TokenType type);
+
+  string document;
+  Tokenizer tokenizer;
+
+  OperationDefinition parseOperationDefinition();
+  SelectionSet parseSelectionSet();
+  Selection parseSelection();
+  Field* parseField();
+
+  string parseName();
+};
+
+Document parse(const char* source);
