fc2090c2e352dc03786abfbe5cdb0e3b4994779e
codingwith3dv <deveshvdddas@gmail.com>
2021-12-04 19:15:36 +0500

    :file_folder: Add definitions + declaration for logging a DocumentNode

--- cgql/type/Document.cpp
+++ cgql/type/Document.cpp
@@ -1,5 +1,7 @@
 #include "Document.h"
 
+using std::ostream;
+
 // Field
 Field::Field(
   const string& name,
@@ -22,3 +24,44 @@ Document::Document(
 ): definitions(definitions) {}
 
 Document::~Document() {}
+
+ostream& operator<<(ostream& out, OperationType type) {
+  string outStr;
+  switch(type) {
+    case OperationType::QUERY:
+      outStr = "QUERY";
+      break;
+    case OperationType::MUTATION:
+      outStr = "MUTATION";
+      break;
+    case OperationType::SUBSCRIPTION:
+      outStr = "SUBSCRIPTION";
+      break;
+  }
+  out << outStr;
+  return out;
+}
+
+void printSelectionSet(SelectionSet selectionSet, int level) {
+  for(auto s : selectionSet) {
+    string v;
+    for(int i = 0; i < level; i++) v += "  ";
+    std::visit([&v](Selection&& args) {
+      if(std::holds_alternative<Field*>(args)) {
+        v += std::get<Field*>(args)->getName();
+      }
+    }, s);
+    logger::info(v);
+    Field* selection = std::get<Field*>(s);
+    if(!selection->getSelectionSet().empty()) {
+      printSelectionSet(selection->getSelectionSet(), level + 1);
+    }
+  }
+}
+
+void printDocumentNode(Document &doc) {
+  for(OperationDefinition opDef : doc.getDefinitions()) {
+    logger::info(opDef.getOperationType());
+    printSelectionSet(opDef.getSelectionSet(), 0);
+  }
+}
