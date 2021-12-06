1a1912dbc61872cb7563c4428fafff36d422e548
codingwith3dv <deveshvdddas@gmail.com>
2021-12-06 13:48:24 +0500

    :sparkles: Add GraphQLSchema for holding queries, mutations and subscriptions

--- cgql/schema/GraphQLDefinition.h
+++ cgql/schema/GraphQLDefinition.h
@@ -1,1 +1,18 @@
+#ifndef GRAPHQL_DEFINITION_H
+#define GRAPHQL_DEFINITION_H
 #include "GraphQLObject/GraphQLObject.h"
+
+class GraphQLSchema {
+public:
+  GraphQLSchema(
+    GraphQLObject& query
+  ): query(query) {};
+  ~GraphQLSchema() {};
+  inline const GraphQLObject& getQuery() const {
+    return this->query;
+  }
+private:
+  GraphQLObject query;
+};
+
+#endif
