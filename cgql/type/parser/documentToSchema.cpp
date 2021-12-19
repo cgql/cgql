0697bdc92769ad2a53a721e05f888b7db06fbbba
codingwith3dv <deveshvdddas@gmail.com>
2021-12-19 19:42:40 +0500

    :sparkles: Add typeName caching

--- cgql/type/parser/documentToSchema.cpp
+++ cgql/type/parser/documentToSchema.cpp
@@ -3,52 +3,62 @@
 namespace cgql {
   namespace internal {
     GraphQLScalarTypes DocToSchemaParser::buildType(
-      const string& typeName
+      const string& typeName,
+      const std::unordered_map<std::string, TypeDefinition>& typeMap
     ) {
       if(typeName == "String") {
         return GraphQLTypes::GraphQLString;
       } else if(typeName == "Int") {
         return GraphQLTypes::GraphQLInt;
       }
-      auto it = this->typeMap.find(typeName);
+      auto it = typeMap.find(typeName);
       ObjectTypeDefinition type =
         std::get<ObjectTypeDefinition>(it->second);
-      GraphQLObject builtType = buildObject(typeName, type);
+      GraphQLObject builtType = buildObject(typeName, type, typeMap);
       return cgqlSMakePtr<GraphQLObject>(builtType);
     }
     std::vector<GraphQLField> DocToSchemaParser::buildFields(
-      const ObjectTypeDefinition& objDef
+      const ObjectTypeDefinition& objDef,
+      const std::unordered_map<std::string, TypeDefinition>& typeMap
     ) {
       std::vector<GraphQLField> fields;
       fields.reserve(objDef.getFields().size());
       for(FieldDefinition fieldDef : objDef.getFields()) {
         GraphQLField field;
         field.setName(fieldDef.getName());
-        field.setType(this->buildType(fieldDef.getType()));
+        field.setType(this->buildType(fieldDef.getType(), typeMap));
         fields.push_back(field);
       }
       return fields;
     }
     GraphQLObject DocToSchemaParser::buildObject(
       const string& name,
-      const ObjectTypeDefinition& typeDef
+      const ObjectTypeDefinition& typeDef,
+      const std::unordered_map<std::string, TypeDefinition>& typeMap
     ) {
       GraphQLObject obj;
       obj.setName(name);
-      obj.getMutableFields() = buildFields(typeDef);
+      obj.getMutableFields() = buildFields(typeDef, typeMap);
+      this->typeNameCache.push_back(name);
       return obj;
     }
     GraphQLSchema DocToSchemaParser::docToSchemaImpl(
       const std::unordered_map<std::string, TypeDefinition>& typeMap
     ) {
-      this->typeMap = typeMap;
       GraphQLSchema schema;
+      this->typeNameCache.reserve(typeMap.size());
       for(auto const& [ key, value ] : typeMap) {
         if(value.index() == 0) {
+          if(std::find(
+            this->typeNameCache.begin(),
+            this->typeNameCache.end(),
+            key
+          ) != this->typeNameCache.end()) continue;
           GraphQLObject obj;
           obj = buildObject(
             key,
-            std::get<ObjectTypeDefinition>(value)
+            std::get<ObjectTypeDefinition>(value),
+            typeMap
           );
           if(obj.getName() == "Query") {
             schema.setQuery(obj);
