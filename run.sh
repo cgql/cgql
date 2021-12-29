6f258077fd6bde7f543e68a5d79d6e655b2584f1
codingwith3dv <deveshvdddas@gmail.com>
2021-12-29 11:28:25 +0500

    :wrench: Update run command

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ fi
 function build() {
   cmake -B build/ -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1
-  cmake --build build/ --config $BUILD_TYPE
+  cd build && make -j4 && cd ..
   chmod +x build/tests/cgqlTests
   time build/tests/cgqlTests
 }
