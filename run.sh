b86183dcc1a5915b1226b32225c1494354540858
codingwith3dv <deveshvdddas@gmail.com>
2022-03-04 10:18:03 +0500

    :wrench: Update development script

--- run.sh
+++ run.sh
@@ -6,9 +6,15 @@ if [ ! -d build ]; then
   mkdir build
 fi
 
+if [[ $1 != "" ]] && [[ $1 = "-c" ]]; then
+  cmake -B build/ \
+    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    -DCMAKE_C_COMPILER=clang \
+    -DCMAKE_CXX_COMPILER=clang++
+fi
+
 function build() {
-  cmake -B build/ -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
   cd build && make -j4 && cd ..
   chmod +x build/tests/cgqlTests
   time build/tests/cgqlTests
